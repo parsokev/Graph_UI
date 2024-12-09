2e1436fbcee7c73e6060c1d0b034594d7269bc29
Kevin <parsokev@oregonstate.edu>
2024-12-08 16:55:03 -0800

    added preprocessor macro for handling VS compiler warnings to all cpp files for use in obj file compilation

--- src/pair_minheap.cpp
+++ src/pair_minheap.cpp
@@ -1,1 +1,6 @@
+// Suppress non-critical compiler warnings if using Microsoft Visual Studio compiler
+#ifdef VS_PRESENT
+#pragma warning(disable : 4265 4625 4626 4688 4710 4711 4866 4820 5045 )
+#endif
+
 #include "pair_minheap.h"
